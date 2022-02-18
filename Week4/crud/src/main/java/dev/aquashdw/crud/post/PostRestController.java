ec9c296ce915a4ac7976cbeca3535514be49426e
hkhh972123 <hkhh972123@gmail.com>
2022-02-18 22:13:00 +0900

    [Week4] Add Repository and Service

--- Week4/crud/src/main/java/dev/aquashdw/crud/post/PostRestController.java
+++ Week4/crud/src/main/java/dev/aquashdw/crud/post/PostRestController.java
@@ -2,6 +2,7 @@ package dev.aquashdw.crud.post;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,29 +13,31 @@ import java.util.List;
 @RequestMapping("post")
 public class PostRestController {
     private static final Logger logger = LoggerFactory.getLogger(PostRestController.class);
-    private final List<PostDto> postList;
+    private final PostService postService;
 
-    public PostRestController(){
-        this.postList = new ArrayList<>();
+    public PostRestController(
+            @Autowired PostService postService
+    ){
+        this.postService = postService;
     }
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     public void createPost(@RequestBody PostDto postDto){
         logger.info(postDto.toString());
-        this.postList.add(postDto);
+        this.postService.createPost(postDto);
     }
 
     @GetMapping()
     public List<PostDto> readPostAll(){
         logger.info("in read post all");
-        return this.postList;
+        return this.postService.readPostAll();
     }
 
     @GetMapping("{id}")
     public PostDto readPost(@PathVariable("id") int id){
         logger.info("in read post");
-        return this.postList.get(id);
+        return this.postService.readPost(id);
     }
 
     @PutMapping("{id}")
@@ -43,19 +46,14 @@ public class PostRestController {
             @PathVariable("id") int id,
             @RequestBody PostDto postDto
     ){
-        PostDto targetPost = this.postList.get(id);
-        if (postDto.getTitle() != null){
-            targetPost.setTitle(postDto.getTitle());
-        }
-        if (postDto.getContent() != null){
-            targetPost.setContent(postDto.getContent());
-        }
-        this.postList.set(id, targetPost);
+        logger.info("target id: " + id);
+        logger.info("update content " + postDto);
+        this.postService.updatePost(id, postDto);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public void deletePost(@PathVariable("id") int id) {
-        this.postList.remove(id);
+        this.postService.deletePost(id);
     }
 }
