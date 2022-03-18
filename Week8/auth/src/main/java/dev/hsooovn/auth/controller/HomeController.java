93f6ecbd3a376488cc6869158addce764e9d5ba3
hkhh972123 <hkhh972123@gmail.com>
2022-03-18 23:13:46 +0900

    Add sign up

--- Week8/auth/src/main/java/dev/hsooovn/auth/controller/HomeController.java
+++ Week8/auth/src/main/java/dev/hsooovn/auth/controller/HomeController.java
@@ -1,19 +1,58 @@
 package dev.hsooovn.auth.controller;
 
+import dev.hsooovn.auth.infra.AuthenticationFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Configuration
 @RequestMapping("home")
 public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    private final AuthenticationFacade authenticationFacade;
 
+    public HomeController(
+            @Autowired AuthenticationFacade authenticationFacade) {
+        this.authenticationFacade = authenticationFacade;
+    }
+
+    // 방법 1
+//    @GetMapping
+//    public String home(Principal principal){
+//        try {
+//            logger.info("connected user : {}", principal.getName());
+//        } catch (NullPointerException e) {
+//            logger.info("no user logged in");
+//        }
+//        return "index";
+//    }
+
+    // 방법 2
+//    @GetMapping
+//    public String home(Authentication authentication){
+//        try {
+//            logger.info("connected user: {}",authentication.getName());
+//        } catch (NullPointerException e) {
+//            logger.info("no user logged in");
+//        }
+//        return "index";
+//    }
+
+    // 방법 3
     @GetMapping
     public String home(){
+        try {
+            logger.info("connected user: {}", authenticationFacade.getUserName());
+        } catch (NullPointerException e) {
+            logger.info("no user logged in");
+        }
         return "index";
     }
 }
--- Week8/auth/src/main/java/dev/hsooovn/auth/controller/UserController.java
+++ Week8/auth/src/main/java/dev/hsooovn/auth/controller/UserController.java
@@ -1,15 +1,58 @@
 package dev.hsooovn.auth.controller;
 
+import dev.hsooovn.auth.entity.UserEntity;
+import dev.hsooovn.auth.entity.UserRepository;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.logging.Logger;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(
+            @Autowired UserRepository userRepository,
+            @Autowired PasswordEncoder passwordEncoder
+            ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @GetMapping("login")
     public String login(){
         return "login-form";
+    }
+
+    @GetMapping("signup")
+    public String signUp(){
+        return "signup-form";
+    }
+
+    @PostMapping("signup")
+    private String signupPost(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            @RequestParam("password_check") String passwordCheck
+    ){
+        if(!password.equals(passwordCheck)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        UserEntity newUser = new UserEntity();
+        newUser.setUsername(username);
+        newUser.setPassword(passwordEncoder.encode(password));
+        userRepository.save(newUser);
+
+        return "redirect:/home";
     }
 }
