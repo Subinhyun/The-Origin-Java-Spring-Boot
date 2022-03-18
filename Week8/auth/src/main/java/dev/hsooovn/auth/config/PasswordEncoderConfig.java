6870af58384f734b8a98b51bfbc02adfea12b48f
hkhh972123 <hkhh972123@gmail.com>
2022-03-18 13:46:30 +0900

    add user entity

---
+++ Week8/auth/src/main/java/dev/hsooovn/auth/config/PasswordEncoderConfig.java
@@ -0,0 +1,12 @@
+package dev.hsooovn.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
--- Week8/auth/src/main/java/dev/hsooovn/auth/config/WebSecurityConfig.java
+++ Week8/auth/src/main/java/dev/hsooovn/auth/config/WebSecurityConfig.java
@@ -1,13 +1,41 @@
 package dev.hsooovn.auth.config;
 
+import dev.hsooovn.auth.infra.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userDetailsService;
+
+    public WebSecurityConfig(@Autowired CustomUserDetailsService customUserDetailsService) {
+        userDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService);
+//        auth.inMemoryAuthentication()
+//                .withUser("user1")
+//                .password(passwordEncoder().encode("user1pass"))
+//                .roles("USER")
+//                .and()
+//                .withUser("admin1")
+//                .password(passwordEncoder().encode("admin1"))
+//                .roles("ADMIN")
+//        ;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
